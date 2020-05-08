4cddf200e9d95973918eb2d2b7d781eddd91b45b
baixixi-0612 <864826053@qq.com>
2020-05-08 18:44:09 +0800

    webpack自动化打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+const path = require("path");
 module.exports = {
-    mode: 'development'
+    mode: "development",
+    //设置入口文件路径
+    entry: path.join(__dirname, "./src/"),
+    //设置出口文件
+    output: {
+        //设置路径
+        path: path.join(__dirname, "./dist"),
+        //设置文件名
+        filename: "bundle.js"
+    }
 }
