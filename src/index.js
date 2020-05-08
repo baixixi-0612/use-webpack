9dd9e7a01d87744dc42fd69d51b3868d626f2d88
baixixi-0612 <864826053@qq.com>
2020-05-08 22:35:16 +0800

    所有loader安装器

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import $ from 'jquery'
+import './css/style.less'
+import './css/style.css'
+import './css/style.scss'
 $(function () {
     $('li:odd').css('backgroundColor', 'blue')
     $('li:even').css('backgroundColor', 'lightblue')
